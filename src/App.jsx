f155c482668de86f33e0f4f13a944355a53e5684
Jorge Rodriguez <alkanware@gmail.com>
2023-10-10 16:50:39 -0500

    Assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import Nav from "./components/Nav"
+
+const  App = ()  => (
+  <main className="relative">
+    <Nav />
+
+  </main>
+)
+
+export default App;
