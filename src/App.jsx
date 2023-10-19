3e5e1ad047a4aa06aaadfcc4f9e1366175c40d12
Jorge Rodriguez <alkanware@gmail.com>
2023-10-19 08:48:04 -0500

    Added ShoeCard Specialoffer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import Nav from "./components/Nav"
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SpecialOffer from "./sections/SpecialOffer";
+import SuperQuality from "./sections/SuperQuality";
 
 const  App = ()  => (
   <main className="relative">
     <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
 
   </main>
 )
